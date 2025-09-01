85d4d24c006af00809d3d05952573b104b369132
onmywaymedia1-web <onmywaymedia1@gmail.com>
2025-08-31 23:31:49 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,18 @@
-self.addEventListener('install', e=>{self.skipWaiting();});
-self.addEventListener('fetch', e=>{});
+// super-simple offline cache
+const CACHE = 'nextlevel-static-v1';
+const ASSETS = ['./','./index.html','./app.jsx','./manifest.json',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js',
+  'https://cdn.tailwindcss.com'
+];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(caches.match(e.request).then(r=>r || fetch(e.request)));
+});
